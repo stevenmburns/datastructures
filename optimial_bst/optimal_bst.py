c073d2979a221ea787a26479c517f99f634312ae
Steven Burns <smburns47@gmail.com>
2021-10-20 18:34:17 -0700

    Checking costs

--- optimial_bst/optimal_bst.py
+++ optimial_bst/optimal_bst.py
@@ -1,4 +1,5 @@
 from itertools import accumulate
+import random
 
 class Tree:
     def __init__(self, key, l=None, r=None):
@@ -9,23 +10,19 @@ class Tree:
     def __eq__(self, other):
         return other is not None and self.key == other.key and self.l == other.l and self.r == other.r
 
-    def find(self, key):
+    def find(self, key, level):
         if key == self.key:
-            return self
+            return self, level
         elif key < self.key:
             if self.l is not None:
-                return self.l.find(key)
+                return self.l.find(key, level+1)
             else:
-                return None
+                return None, level
         else:
             if self.r is not None:
-                return self.r.find(key)
+                return self.r.find(key, level+1)
             else:
-                return None
-            if self.l is not None:
-                return self.l.find(key)
-            else:
-                return None
+                return None, level
 
     @staticmethod
     def build(keys, probs):
@@ -41,19 +38,16 @@ class Tree:
             # (0,0),(1,1),(2,2),(3,3)
             costs[(i,i)] = 0, None
 
-        for i in range(len(keys)):
-            # (0,1),(1,2),(2,3)
-            costs[(i,i+1)] = probs[i], i
-
-        # j in {1,2}
-        for j in range(1,len(keys)):
-            # i in {0,1} for j == 1
-            # i in {0}   for j == 2
+        # j in {0,1,2}
+        for j in range(len(keys)):
+            # i in {0,1,2} for j == 0
+            # i in {0,1}   for j == 1
+            # i in {0}     for j == 2
             for i in range(len(keys)-j):
+                # (0,1),(1,2),(2,3)
                 #    (0,2),(1,3)
                 #       (0,3)
                 ii = i + j + 1
-                # for (0,2) we need (0,0) 0 (1,2); (0,1) 1 (2,2)
 
                 # probs =  0.3 0.2 0.5
                 # keys  =  0   1   2   3  
@@ -61,6 +55,7 @@ class Tree:
                 # probs between (0,2) cprob[2] - cprob[0]
                 # probs[k] = cprobs[k+1] - cprobs[k]
 
+                # for (i,ii) = (0,2) we need (0,0) 0 (1,2); (0,1) 1 (2,2)
                 lst = []
                 for k in range(i,ii):
                     cand2 = costs[(i,k)][0]    + cprobs[k]  - cprobs[i]   + \
@@ -70,8 +65,9 @@ class Tree:
                            costs[(k+1,ii)][0] + cprobs[ii]
                     assert abs(cand - cand2) <= 0.00001
                         
-                    lst.append( (cand, k))
-                costs[(i,ii)] = min(lst)
+                    #lst.append( (cand, k))
+                    lst.append( (cand, Tree(k, costs[(i,k)][1], costs[(k+1,ii)][1])))
+                costs[(i,ii)] = min(lst, key=lambda p: p[0])
                 
         def aux(lb, ub):
             if lb < ub:
@@ -82,7 +78,8 @@ class Tree:
 
         print(costs[(0,len(keys))])
 
-        return aux(0,len(keys))
+        #return aux(0,len(keys))
+        return costs[(0,len(keys))]
 
     def __repr__(self):
         return f'Tree({self.key}, {repr(self.l)}, {repr(self.r)})'
@@ -90,18 +87,40 @@ class Tree:
 def test_A():
     t = Tree(1, Tree(0), Tree(2)) 
     for x in range(3):
-        assert t.find(x) is not None
+        assert t.find(x,1) is not None
 
 def test_B():
 
-    t = Tree.build( [0], [1.0])
+    _, t = Tree.build( [0], [1.0])
     assert t == Tree(0)
 
-    t = Tree.build( [0,1], [.3,.7])
+    _, t = Tree.build( [0,1], [.3,.7])
     assert t == Tree(1, Tree(0), None)
 
-    t = Tree.build( [0,1], [.7,.3])
+    _, t = Tree.build( [0,1], [.7,.3])
     assert t == Tree(0, None, Tree(1))
 
-    t = Tree.build( [0,1,2], [.3,.2,.5])
+    _, t = Tree.build( [0,1,2], [.3,.2,.5])
     assert t == Tree(2, Tree(0, None, Tree(1)))
+
+def test_C():
+    random.seed(47)
+
+    n = 100
+
+    freqs = [ random.randrange(1, 100) for _ in range(n)]
+    s = sum(freqs)
+    probs = [ x / s for x in freqs]
+    keys = list(range(n))
+
+    c, t = Tree.build( keys, probs)
+
+    print( list(zip(keys,freqs)))
+
+    r = 0
+    for x,p in zip(keys,probs):
+        _, l = t.find(x, 1)
+        r += l*p
+
+    print(r)
+    assert abs(c-r) <= 0.00001
