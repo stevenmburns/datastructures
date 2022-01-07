8b63c7f2a79665d7fe5b5da9ad841d684d639bf0
Steven Burns <smburns47@gmail.com>
2022-01-06 20:40:43 -0800

    Format output better

--- expand-restrictions/test_er.py
+++ expand-restrictions/test_er.py
@@ -17,9 +17,23 @@ class Restriction:
     ored_terms: List[OredTerm]
 
     def check(self, x, scaling):
-        x0 = x % self.period
+        return any((x % self.period) in ored_term.offsets and
+                   scaling in ored_term.scalings
+                   for ored_term in self.ored_terms)
 
-        return any(x0 in ored_term.offsets and scaling in ored_term.scalings for ored_term in self.ored_terms)
+
+def simplify(pairs):
+    offsets_with_pos_scaling = {o for o, s in pairs if s == 1}
+    offsets_with_neg_scaling = {o for o, s in pairs if s == -1}
+    offsets_with_both_scalings = offsets_with_pos_scaling.intersection(offsets_with_neg_scaling)
+    offsets_with_pos_scaling -= offsets_with_both_scalings
+    offsets_with_neg_scaling -= offsets_with_both_scalings
+    if offsets_with_both_scalings:
+        yield OredTerm(list(sorted(list(offsets_with_both_scalings))), [1, -1])
+    if offsets_with_pos_scaling:
+        yield OredTerm(list(sorted(list(offsets_with_pos_scaling))), [1])
+    if offsets_with_neg_scaling:
+        yield OredTerm(list(sorted(list(offsets_with_neg_scaling))), [-1])
 
 
 def merge(r0, r1):
@@ -32,15 +46,12 @@ def merge(r0, r1):
                 for offset, scaling in product(ored_term.offsets, ored_term.scalings):
                     yield (coarse_offset + offset, scaling)
 
-    ored_terms0 = set(gen_pairs(r0))
-    ored_terms1 = set(gen_pairs(r1))
+    pairs0 = set(gen_pairs(r0))
+    pairs1 = set(gen_pairs(r1))
 
-    print(f'ored_terms0: {ored_terms0}')
-    print(f'ored_terms1: {ored_terms1}')
+    pairs = pairs0.intersection(pairs1)
 
-    ored_terms = ored_terms0.intersection(ored_terms1)
-
-    return Restriction(new_period, [OredTerm([o], [s]) for o, s in ored_terms])
+    return Restriction(new_period, list(simplify(pairs)))
 
 
 def test_A0():
@@ -73,5 +84,12 @@ def test_A2():
 def test_A3():
     r0 = Restriction(4, [OredTerm([0, 1], [1])])
     r1 = Restriction(6, [OredTerm([0, 1, 2], [1])])
+    r = merge(r0, r1)
+    print(r)
+
+
+def test_A4():
+    r0 = Restriction(2, [OredTerm([0], [1])])
+    r1 = Restriction(3, [OredTerm([0, 1], [1])])
     r = merge(r0, r1)
     print(r)
