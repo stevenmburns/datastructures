71e485b4fbd4b1aaf4e9c88428eb064015f652fa
Steven Burns <smburns47@gmail.com>
2022-01-09 12:24:24 -0800

    Shortest.

--- robot-bounded-in-circle/test_rbic.py
+++ robot-bounded-in-circle/test_rbic.py
@@ -1,31 +1,19 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        ds = [(0, 1), (-1, 0), (0, -1), (1, 0)]
         x, y = 0, 0
-        Ss = "NWSE"
-        Ls = Ss[1:] + Ss[:1]
-        Rs = Ss[:1] + Ss[1:]
-
-        ds = {"N": (0, 1), "W": (-1, 0), "S": (0, -1), "E": (1, 0)}
-
-        def LL(d):
-            return Ls[Ss.index(d)]
-
-        def RR(d):
-            return Rs[Ss.index(d)]
-
-        d = 'N'
+        d = 0
         for c in instructions:
             if c == 'G':
                 dx, dy = ds[d]
                 x, y = x + dx, y + dy
             elif c == 'L':
-                d = LL(d)
+                d = (d + 1) % 4
             elif c == 'R':
-                d = RR(d)
+                d = (d - 1) % 4
             else:
                 assert False, c
-
-        return (x, y) == (0, 0) or d in "WSE"
+        return (x, y) == (0, 0) or d != 0
 
 
 def test_A0():
