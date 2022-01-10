732cc71a88b5821b526e685643b632b7f647cbfe
Steven Burns <smburns47@gmail.com>
2022-01-10 10:00:51 -0800

    Added hypothesis tests

--- max_twin_sum/test_mts.py
+++ max_twin_sum/test_mts.py
@@ -3,6 +3,9 @@
 from typing import List, Optional
 from dataclasses import dataclass
 
+from hypothesis import given, example
+from hypothesis.strategies import integers, lists
+
 
 @dataclass
 class ListNode:
@@ -12,41 +15,71 @@ class ListNode:
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+        assert head is not None, 'No empty list'
 
-        slow, fast, prev = head, head, None
+        header = ListNode(0, head)
+
+        slow, fast = header, head
 
         while fast is not None and fast.next is not None:
-            prev = slow
             slow = slow.next
             fast = fast.next.next
 
-        prev.next = None
+        assert fast is None, 'No odd number of nodes'
 
-        reverse_head = None
-        while slow is not None:
-            temp = slow.next
-            slow.next = reverse_head
-            reverse_head = slow
-            slow = temp
+        def reverse_list(u):
+            reverse_head = None
+            while u is not None:
+                u_next = u.next
+                u.next = reverse_head
+                reverse_head = u
+                u = u_next
+            return reverse_head
 
-        u0, u1 = head, reverse_head
-        m = None
-        while u0 is not None and u1 is not None:
-            cand = u0.val + u1.val
-            if m is None or m < cand:
-                m = cand
-            u0, u1 = u0.next, u1.next
+        def traverse(u):
+            while u is not None:
+                yield u.val
+                u = u.next
 
-        return m
+        head2 = slow.next
+        slow.next = None
 
+        reversed_head2 = reverse_list(head2)
 
-def test_A0():
-    assert Solution().pairSum(ListNode(5, ListNode(4, ListNode(2, ListNode(1))))) == 6
+        res = max((a+b for a, b in zip(traverse(head), traverse(reversed_head2))))
 
+        slow.next = reverse_list(reversed_head2)
 
-def test_A1():
-    assert Solution().pairSum(ListNode(4, ListNode(2, ListNode(2, ListNode(3))))) == 7
+        return res
 
 
-def test_A2():
-    assert Solution().pairSum(ListNode(1, ListNode(100000))) == 100001
+def toList(head: Optional[ListNode]) -> List[int]:
+    res = []
+    while head is not None:
+        res.append(head.val)
+        head = head.next
+    return res
+
+
+def fromList(l: List[int]) -> Optional[ListNode]:
+    if len(l) == 0:
+        return None
+    head = ListNode(l[0])
+    curr = head
+    for i in l[1:]:
+        curr.next = ListNode(i)
+        curr = curr.next
+    return head
+
+
+@example([1, 100000])
+@example([4, 2, 2, 3])
+@example([5, 4, 2, 1])
+@given(lists(integers(min_value=0, max_value=10), min_size=2, max_size=100).filter(lambda l: len(l) % 2 == 0))
+def test_hypo(lst):
+    print(lst)
+    n = len(lst)
+    head = fromList(lst)
+    golden = max((lst[i] + lst[n-i-1] for i in range(n//2)))
+    assert Solution().pairSum(head) == golden
+    assert lst == toList(head)
