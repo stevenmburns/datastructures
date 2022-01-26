2b4d7545a30af3ffbcdec67d8cce86e62d884b95
Steven Burns <smburns47@gmail.com>
2022-01-26 02:13:00 -0800

    Before refactoring

--- different-ways-to-add-parentheses/test_dw.py
+++ different-ways-to-add-parentheses/test_dw.py
@@ -1,20 +1,18 @@
 from typing import List
+from operator import __add__, __sub__, __mul__
+from itertools import product
 
 
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-
         tokens = []
-
         num = None
-
         for c in expression:
-
             if c in '+-*':
                 assert num is not None
                 tokens.append(num)
-                tokens.append(c)
                 num = None
+                tokens.append(c)
             else:
                 if num is None:
                     num = ord(c) - ord('0')
@@ -23,26 +21,19 @@ class Solution:
         if num is not None:
             tokens.append(num)
 
-        tbl = {}
+        ops = {'+': __add__, '-': __sub__, '*': __mul__}
 
+        n = (len(tokens)+1) // 2
+
+        tbl = {}
         for i in range(0, len(tokens), 2):
             tbl[(i, i)] = {tokens[i]}
 
         for k in range(2, len(tokens), 2):
-            for i in range(0, len(tokens), 2):
+            for i in range(0, len(tokens)-k, 2):
                 j = i + k
-                if 0 <= j < len(tokens):
-                    s = set()
-                    for l in range(i, j, 2):
-                        a, b = tbl[(i, l)], tbl[(l+2, j)]
-                        op = tokens[l+1]
-                        if op == '+':
-                            s.update(u+v for u in a for v in b)
-                        elif op == '-':
-                            s.update(u-v for u in a for v in b)
-                        elif op == '*':
-                            s.update(u*v for u in a for v in b)
-                    tbl[(i, j)] = s
+                tbl[(i, j)] = [ops[tokens[l+1]](u, v) for l in range(i, j, 2)
+                               for u, v in product(tbl[(i, l)], tbl[(l+2, j)])]
 
         return tbl[(0, len(tokens)-1)]
 
@@ -53,3 +44,17 @@ def test_A0():
 
 def test_A1():
     assert sorted(Solution().diffWaysToCompute("2*3-4*5")) == sorted([-34, -10, -14, -10, 10])
+
+
+def test_A2():
+    assert sorted(
+        Solution().diffWaysToCompute("2*3-4*5*6+7")) == sorted(
+        [-514, -514, -442, -442, -290, -254, -254, -248, -248, -227, -227, -220, -220, -197, -197, -190, -182, -142,
+         -130, -130, -130, -130, -130, -121, -121, -107, -107, -77, -74, -74, -53, -53, -53, -53, -53, -46, -46, 67, 67,
+         74, 130, 130])
+
+
+def test_A3():
+    assert sorted(
+        Solution().diffWaysToCompute("2*3-4*5*6")) == sorted(
+        [-234, -234, -204, -204, -114, -114, -84, -60, -60, -60, -60, -60, 60, 60])
